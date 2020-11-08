d8eb9dec3c7293aa742fcdec880e3cecb020d5e0
Yuniya Kim <kim.yuniya@mail.ru>
2020-11-08 17:13:00 +0300

    Added some variables

---
+++ Semester5/LaboratoryWork4/LaboratoryWork4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaboratoryWork4
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Semester5/LaboratoryWork4/LaboratoryWork4/QuadratureFormula.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LaboratoryWork4
+{
+    /// <summary>
+    /// Quadrature formulas
+    /// </summary>
+    public class QuadratureFormula
+    {
+        private double left;
+        private double right;
+        private int partition;
+
+        /// <summary>
+        /// Function of zero degree polynomial
+        /// </summary>
+        /// <param name="x">Argument</param>
+        /// <returns>Value of function</returns>
+        private double ZeroDegreePolynomailFunction(double x) => 6;
+
+        /// <summary>
+        /// Function of first degree polynomial
+        /// </summary>
+        /// <param name="x">Argument</param>
+        /// <returns>Value of function</returns>
+        private double FirstDegreePolynomailFunction(double x) => 4 * x + 8;
+
+        /// <summary>
+        /// Function of second degree polynomial
+        /// </summary>
+        /// <param name="x">Argument</param>
+        /// <returns>Value of function</returns>
+        private double SecondDegreePolynomailFunction(double x) => 3 * x * x - 2 * x + 7;
+
+        /// <summary>
+        /// Function of third degree polynomial
+        /// </summary>
+        /// <param name="x">Argument</param>
+        /// <returns>Value of function</returns>
+        private double ThirdDegreePolynomailFunction(double x) => x * x * x - 2 * x * x + 4 * x - 10;
+
+        /// <summary>
+        /// Function of weight
+        /// </summary>
+        /// <param name="x">Argument</param>
+        /// <returns>Value of function</returns>
+        private double WeightFunction(double x) => 1;
+    }
+}
