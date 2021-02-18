4904f589c869cd9b856513b951cb727d3d94f51e
Yuniya Kim <kim.yuniya@mail.ru>
2021-02-18 12:49:01 +0300

    Added working example

--- Semester6/Task1/Task1/ConditionNumber.cs
+++ Semester6/Task1/Task1/ConditionNumber.cs
@@ -10,14 +10,13 @@ namespace Task1
     /// </summary>
     public class ConditionNumber
     {
-        private DotNumerics.LinearAlgebra.Matrix matrix;
+        private Matrix matrix;
         private Vector vector;
-        private int size;
         private Vector exactSolution;
 
-        //public double SpectralCriterion { get; private set; }
-        //public double VolumetricCriterion { get; private set; }
-        //public double AngleCriterion { get; private set; }
+        public int size { get; private set; }
+
+        public ConditionNumber() { }
 
         /// <summary>
         /// Condition number's constructor
@@ -25,7 +24,7 @@ namespace Task1
         /// <param name="matrix">Given matrix</param>
         /// <param name="vector">Given vector</param>
         /// <param name="exactSolution">Given exact solution</param>
-        public ConditionNumber(DotNumerics.LinearAlgebra.Matrix matrix, Vector vector, Vector exactSolution)
+        public ConditionNumber(Matrix matrix, Vector vector, Vector exactSolution)
         {
             this.matrix = matrix;
             this.vector = vector;
@@ -116,6 +115,65 @@ namespace Task1
 
             var solver = new LinearEquations();
             return solver.Solve(variedMatrix, variedVector);
+        }
+
+        public void Start()
+        {
+            var equations = new List<(Matrix, Vector, Vector)>();
+            Matrix leftMatrix = new Matrix(2, 2);
+            leftMatrix[0, 0] = -400.6;
+            leftMatrix[0, 1] = 199.8;
+            leftMatrix[1, 0] = 1198.8;
+            leftMatrix[1, 1] = -600.4;
+            var rightMatrix = new Vector(new double[] { 200, -600 });
+            var exactSolution = new Vector(new double[] { -0.2, 0.6 });
+            equations.Add((leftMatrix, rightMatrix, exactSolution));
+
+            foreach (var equation in equations)
+            {
+                var conditionNumber = new ConditionNumber(equation.Item1, equation.Item2, equation.Item3);
+                Console.WriteLine("Left matrix is");
+                Console.WriteLine(equation.Item1.MatrixToString());
+                Console.WriteLine("Right matrix is");
+                Console.WriteLine(equation.Item2[0]);
+                Console.WriteLine(equation.Item2[1]);
+                Console.WriteLine();
+                Console.WriteLine("Exact solution is");
+                Console.WriteLine(equation.Item3[0]);
+                Console.WriteLine(equation.Item3[1]);
+
+                var spectralCriterion = conditionNumber.CalculateSpectralCriterion();
+                var volumetricCriterion = conditionNumber.CalculateVolumetricCriterion();
+                var angleCriterion = conditionNumber.CalculateAngleCriterion();
+                Console.WriteLine();
+                Console.WriteLine($"Spectral criterion is {spectralCriterion}.");
+                Console.WriteLine($"Volumetric criterion is {volumetricCriterion}.");
+                Console.WriteLine($"Angle criterion is {angleCriterion}.");
+
+                var variedSolution2 = conditionNumber.CalculateVariedSolution(0.01);
+                var variedSolution5 = conditionNumber.CalculateVariedSolution(0.00001);
+                var variedSolution8 = conditionNumber.CalculateVariedSolution(0.00000001);
+                var difference2 = equation.Item3 - variedSolution2;
+                var difference5 = equation.Item3 - variedSolution5;
+                var difference8 = equation.Item3 - variedSolution8;
+                double delta2 = 0;
+                double delta5 = 0;
+                double delta8 = 0;
+                for (var i = 0; i < conditionNumber.size; i++)
+                {
+                    delta2 += difference2[i] * difference2[i];
+                    delta5 += difference5[i] * difference5[i];
+                    delta8 += difference8[i] * difference8[i];
+                }
+                delta2 = Math.Sqrt(delta2);
+                delta5 = Math.Sqrt(delta5);
+                delta8 = Math.Sqrt(delta8);
+
+                Console.WriteLine();
+                Console.WriteLine($"|x - x˜| is {delta2} when variation is 0.01.");
+                Console.WriteLine($"|x - x˜| is {delta5} when variation is 0.00001.");
+                Console.WriteLine($"|x - x˜| is {delta8} when variation is 0.00000001.");
+            }
         }
     }
 }
--- Semester6/Task1/Task1/Program.cs
+++ Semester6/Task1/Task1/Program.cs
@@ -2,11 +2,12 @@
 
 namespace Task1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var cn = new ConditionNumber();
+            cn.Start();
         }
     }
 }
