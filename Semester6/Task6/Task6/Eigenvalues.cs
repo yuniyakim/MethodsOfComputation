29ed8e1349017b021d8a22ab09ded11021851744
Yuniya Kim <kim.yuniya@mail.ru>
2021-09-13 21:55:37 +0300

    Added some matrices

--- Semester6/Task6/Task6/Eigenvalues.cs
+++ Semester6/Task6/Task6/Eigenvalues.cs
@@ -67,6 +67,35 @@ namespace Task6
         }
 
         /// <summary>
+        /// Defines if eigenvalue belongs to Gershgorin circles
+        /// </summary>
+        /// <param name="aMatrix">Matrix</param>
+        /// <param name="eigenvalue">Eigenvalue</param>
+        /// <returns>True if eigenvalue belongs to Gershgorin circles, false otherwise</returns>
+        public bool IsInGershgorinCircle(Matrix<double> aMatrix, double eigenvalue)
+        {
+            var circles = new List<(double, double)>();
+            for (var i = 0; i < aMatrix.RowCount; i++)
+            {
+                double sum = 0;
+                for (var j = 0; j < aMatrix.ColumnCount; j++)
+                {
+                    sum += Math.Abs(aMatrix[i, j]);
+                }
+                circles.Add((aMatrix[i, i], sum - Math.Abs(aMatrix[i, i])));
+            }
+
+            foreach (var circle in circles)
+            {
+                if (Math.Abs(circle.Item1 - eigenvalue) <= circle.Item2)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
         /// Calculates eigenvalues with Jacobi method
         /// </summary>
         /// <param name="epsilon">Epsilon</param>
@@ -103,12 +132,12 @@ namespace Task6
                         j += 2;
                     }
                 }
-                if ((i == currentMatrix.RowCount - 1 && j == currentMatrix.RowCount) || Math.Abs(currentMatrix[i, j]) < epsilon)
+                if (Math.Abs(currentMatrix[i, j]) < epsilon)
                 {
                     return (currentMatrix.Diagonal(), amount);
                 }
                 amount++;
-                var phi = 1 / 2 * Math.Atan(2 * currentMatrix[i, j] / (currentMatrix[i, i] - currentMatrix[j, j]));
+                var phi = Math.Atan(2 * currentMatrix[i, j] / (currentMatrix[i, i] - currentMatrix[j, j])) / 2;
                 hMatrix[i, i] = Math.Cos(phi);
                 hMatrix[j, j] = Math.Cos(phi);
                 hMatrix[i, j] = -Math.Sin(phi);
@@ -124,63 +153,31 @@ namespace Task6
         {
             var equations = new List<(Matrix<double>, Vector<double>)>();
 
-            var matrix1 = Matrix<double>.Build.Dense(3, 3);
-            matrix1[0, 0] = -198.1;
-            matrix1[0, 1] = 389.9;
-            matrix1[0, 2] = 123.2;
-            matrix1[1, 0] = 0;
-            matrix1[1, 1] = 202.4;
-            matrix1[1, 2] = 249.3;
-            matrix1[2, 0] = 0;
-            matrix1[2, 1] = 0;
-            matrix1[2, 2] = -489.2;
-            var vector1 = Vector<double>.Build.Random(3, 16);
-            equations.Add((matrix1, vector1));
-
-            var matrix2 = Matrix<double>.Build.Dense(5, 5);
-            matrix2[0, 0] = 2;
-            matrix2[0, 1] = -1;
-            matrix2[0, 2] = 0;
-            matrix2[0, 3] = 0;
-            matrix2[0, 4] = 0;
-            matrix2[1, 0] = -3;
-            matrix2[1, 1] = 8;
-            matrix2[1, 2] = -1;
-            matrix2[1, 3] = 0;
-            matrix2[1, 4] = 0;
-            matrix2[2, 0] = 0;
-            matrix2[2, 1] = -5;
-            matrix2[2, 2] = 12;
-            matrix2[2, 3] = 2;
-            matrix2[2, 4] = 0;
-            matrix2[3, 0] = 0;
-            matrix2[3, 1] = 0;
-            matrix2[3, 2] = -6;
-            matrix2[3, 3] = 18;
-            matrix2[3, 4] = -4;
-            matrix2[4, 0] = 0;
-            matrix2[4, 1] = 0;
-            matrix2[4, 2] = 0;
-            matrix2[4, 3] = -5;
-            matrix2[4, 4] = 10;
-            var vector2 = Vector<double>.Build.Random(5, 10);
+            var matrix2 = Matrix<double>.Build.Dense(3, 3);
+            matrix2[0, 0] = -0.81417;
+            matrix2[0, 1] = -0.01937;
+            matrix2[0, 2] = 0.41372;
+            matrix2[1, 0] = -0.01937;
+            matrix2[1, 1] = 0.54414;
+            matrix2[1, 2] = 0.00590;
+            matrix2[2, 0] = 0.41372;
+            matrix2[2, 1] = 0.00590;
+            matrix2[2, 2] = -0.81445;
+            var vector2 = Vector<double>.Build.Random(3, 10);
             equations.Add((matrix2, vector2));
 
-            var matrix3 = Matrix<double>.Build.Dense(2, 2);
-            matrix3[0, 0] = 1;
-            matrix3[0, 1] = 0.99;
-            matrix3[1, 0] = 0.99;
-            matrix3[1, 1] = 0.98;
-            var vector3 = Vector<double>.Build.Random(2, 666);
+            var matrix3 = Matrix<double>.Build.Dense(3, 3);
+            matrix3[0, 0] = -1.51898;
+            matrix3[0, 1] = -0.19907;
+            matrix3[0, 2] = 0.95855;
+            matrix3[1, 0] = -0.19907;
+            matrix3[1, 1] = 1.17742;
+            matrix3[1, 2] = 0.06992;
+            matrix3[2, 0] = 0.95855;
+            matrix3[2, 1] = 0.06992;
+            matrix3[2, 2] = -1.57151;
+            var vector3 = Vector<double>.Build.Random(3, 666);
             equations.Add((matrix3, vector3));
-
-            var matrix31 = Matrix<double>.Build.Dense(2, 2);
-            matrix31[0, 0] = -401.98;
-            matrix31[0, 1] = 200.34;
-            matrix31[1, 0] = 1202.04;
-            matrix31[1, 1] = -602.32;
-            var vector31 = Vector<double>.Build.Random(2, 666);
-            equations.Add((matrix31, vector31));
 
             var matrix4 = Matrix<double>.Build.Dense(4, 4);
             for (var i = 0; i < 4; i++)
@@ -194,33 +191,45 @@ namespace Task6
             var vector4 = Vector<double>.Build.Random(4, 4);
             equations.Add((matrix4, vector4));
 
-            var matrix5 = Matrix<double>.Build.Dense(5, 5);
-            for (var i = 0; i < 5; i++)
-            {
-                for (var j = 0; j < 5; j++)
-                {
-                    matrix5[i, j] = 1;
-                    matrix5[i, j] /= 1 + i + j;
-                }
-            }
-            var vector5 = Vector<double>.Build.Random(5, 5);
-            equations.Add((matrix5, vector5));
+            //var matrix5 = Matrix<double>.Build.Dense(5, 5);
+            //for (var i = 0; i < 5; i++)
+            //{
+            //    for (var j = 0; j < 5; j++)
+            //    {
+            //        matrix5[i, j] = 1;
+            //        matrix5[i, j] /= 1 + i + j;
+            //    }
+            //}
+            //var vector5 = Vector<double>.Build.Random(5, 5);
+            //equations.Add((matrix5, vector5));
 
-            var matrix6 = Matrix<double>.Build.Dense(6, 6);
-            for (var i = 0; i < 6; i++)
+            var matrix6 = Matrix<double>.Build.Dense(15, 15);
+            for (var i = 0; i < 15; i++)
             {
-                for (var j = 0; j < 6; j++)
+                for (var j = 0; j < 15; j++)
                 {
                     matrix6[i, j] = 1;
                     matrix6[i, j] /= 1 + i + j;
                 }
             }
-            var vector6 = Vector<double>.Build.Random(6, 6);
+            var vector6 = Vector<double>.Build.Random(15, 15);
             equations.Add((matrix6, vector6));
+
+            var matrix7 = Matrix<double>.Build.Dense(10, 10);
+            for (var i = 0; i < 10; i++)
+            {
+                for (var j = 0; j < 10; j++)
+                {
+                    matrix7[i, j] = 1;
+                    matrix7[i, j] /= 1 + i + j;
+                }
+            }
+            var vector7 = Vector<double>.Build.Random(10, 10);
+            equations.Add((matrix7, vector7));
 
             foreach (var equation in equations)
             {
-                Console.WriteLine("Power method");
+                Console.WriteLine("Max module eigenvalue strategy");
                 Console.WriteLine(string.Format("{0,-10}|{1,-25}|{2,-25}", "ε", "|lambda - lambda_ε|", "Amount of iterations"));
 
                 matrix = equation.Item1;
@@ -231,25 +240,25 @@ namespace Task6
                 for (var i = -2; i > -6; i--)
                 {
                     var epsilon = Math.Pow(10, i);
-                    var result = CalculateEigenvaluesWithJacobiMethod(vector, epsilon);
+                    var result = CalculateEigenvaluesWithJacobiMethod(epsilon, true);
                     var newEigenvalue = result.Item1;
                     var amount = result.Item2;
-                    var error = Math.Abs(eigenvalue - newEigenvalue);
+                    var error = (newEigenvalue - eigenvalue).L2Norm();
                     Console.WriteLine(string.Format("{0,-10}|{1,-25}|{2,-25}", epsilon, error, amount));
                 }
 
                 Console.WriteLine();
-                Console.WriteLine("Scalar products method");
+                Console.WriteLine("Zeroing in order strategy");
                 Console.WriteLine(string.Format("{0,-10}|{1,-25}|{2,-25}", "ε", "|lambda - lambda_ε|", "Amount of iterations"));
 
                 for (var i = -2; i > -6; i--)
                 {
                     var epsilon = Math.Pow(10, i);
                     size = matrix.RowCount;
-                    var result = CalculateMaxEigenvalueWithScalarProductsMethod(vector, epsilon);
+                    var result = CalculateEigenvaluesWithJacobiMethod(epsilon, false);
                     var newEigenvalue = result.Item1;
                     var amount = result.Item2;
-                    var error = Math.Abs(eigenvalue - newEigenvalue);
+                    var error = (newEigenvalue - eigenvalue).L2Norm();
                     Console.WriteLine(string.Format("{0,-10}|{1,-25}|{2,-25}", epsilon, error, amount));
                 }
 
